20ad98bc08c3f7ede56276c68260b77604d6b1a4
Leandro Martin Soler <martosoler@gmail.com>
2014-09-14 20:38:38 -0300

    Leandro Soler - Initial commit for positional app

--- position-map2/public/src/scripts/app.js
+++ position-map2/public/src/scripts/app.js
@@ -1,15 +1,24 @@
 /** @jsx React.DOM */
 
 var React = require('react');
-var Map = require('react-map/src/map')
+var Map = require('./map.js')
 
-var HelloMessage = React.createClass({
-            render: function () {
-                return ( <Map latitude={60.170833} longitude={24.9375} zoom={5} width={250} height={250}
-  points={[{latitude:60.1876172,longitude:24.815366,title:"HIIT Open Innovation House"},{latitude:60.185478,longitude:24.812257,title:"HIIT Otaniemi"}, {latitude:60.2049747,longitude:24.9634712,title:"HIIT Kumpula"}]} />
-                );
-            }
+var PositionalApp = React.createClass({
+		render: function () {
+			return ( <Map 
+					latitude={60.170833} 
+					longitude={24.9375} 
+					zoom={5} 
+					width={window.screen.availWidth} 
+					height={window.screen.availHeight}
+					points={[
+						{latitude:60.1876172,longitude:24.815366,title:"HIIT Open Innovation House"},
+						{latitude:60.185478,longitude:24.812257,title:"HIIT Otaniemi"},
+						{latitude:60.2049747,longitude:24.9634712,title:"HIIT Kumpula"}]
+					} />
+			);
+		}
     }
 );
 
-module.exports = HelloMessage;
+module.exports = PositionalApp;

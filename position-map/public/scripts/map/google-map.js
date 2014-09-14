b2a50aeaad4dbb4120f048b43838c76496d4f25f
Leandro Martin Soler <martosoler@gmail.com>
2014-09-14 20:39:49 -0300

    Leandro Soler - Removing prior positional app

--- position-map/public/scripts/map/google-map.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by papa on 18/05/14.
- */
-define('google-map', ['async!http://maps.google.com/maps/api/js?v=3&sensor=false'],
-    function(){
-        // return the gmaps namespace for brevity
-        return window.google.maps;
-    }
-);
